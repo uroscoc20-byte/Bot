0ea08c5432baa8872ab1d588e79e1c3a45934994
uroscoc20-byte <uroscoc20@gmail.com>
2025-10-19 20:06:53 +0200

    Add files via upload

--- tickets.py
+++ tickets.py
@@ -371,8 +371,8 @@ class TicketView(View):
         super().__init__(timeout=None)
         self.category = category
         self.requestor_id = requestor_id
-        self.add_item(Button(label="Join", style=discord.ButtonStyle.success, custom_id="join_ticket", emoji="➕"))
-        self.add_item(Button(label="Close", style=discord.ButtonStyle.danger, custom_id="close_ticket", emoji="🔒"))
+        self.add_item(Button(label="Join", style=discord.ButtonStyle.green, custom_id="join_ticket", emoji="➕"))
+        self.add_item(Button(label="Close", style=discord.ButtonStyle.red, custom_id="close_ticket", emoji="🔒"))
 
 class RewardChoiceView(View):
     def __init__(self, ticket_channel_id: int):
