b1f5abf00ff882013753eff0d933a642c92d1e00
uroscoc20-byte <uroscoc20@gmail.com>
2026-02-11 18:21:22 +0100

    Create tickets.py

---
+++ tickets.py
@@ -0,0 +1,22 @@
+"""
+Central hub for ticket system - imports and re-exports all ticket functionality
+"""
+
+import discord
+from tickets_modals import TicketModal
+from tickets_buttons_actions import TicketActionView
+from tickets_buttons_panel import TicketView, DeleteChannelView
+from tickets_commands import setup_tickets
+from tickets_embeds import create_ticket_embed
+from tickets_utils import generate_join_commands
+
+# Re-export everything main.py needs
+__all__ = [
+    'TicketView',
+    'TicketActionView', 
+    'DeleteChannelView',
+    'setup_tickets',
+    'TicketModal',
+    'create_ticket_embed',
+    'generate_join_commands'
+]
